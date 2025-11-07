43868cc15b7af2aad66e69c27d39d17176d3b2df
amnamunawar01 <amnamunawar171@gmail.com>
2025-11-07 10:50:09 -0800

    Add files via upload

---
+++ EmployeeObserver.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package GUI;
+
+/**
+ *
+ * @author LENOVO
+ */
+
+// 🔔 Observer interface for the Observer Pattern
+public interface EmployeeObserver {
+    void onEmployeeDataChanged();
+}
+
+    
