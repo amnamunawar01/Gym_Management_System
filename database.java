92decca9c7c9d6dd49a00975d975472295cdfc92
amnamunawar01 <amnamunawar171@gmail.com>
2025-11-07 10:48:09 -0800

    Add files via upload

--- database.java
+++ database.java
@@ -1,4 +1,3 @@
-
 package data;
 
 import java.sql.Connection;
@@ -6,23 +5,24 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class database {
-    public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException  {
-        
-    String url="jdbc:mysql://localhost:3306/gymdata";
-    String user="root";
-    String password="";
-    String driver="com.mysql.cj.jdbc.Driver";
-    
-    try{
-        Class.forName(driver).newInstance();
-    Connection connection=DriverManager.getConnection(url,user,password);
-    System.out.println("good");
-    } 
-    
-    catch(SQLException e){
-    System.out.println("bad");
+    public static void main(String[] args) {
+        System.out.println("Starting program..."); // Test line
+
+        String url = "jdbc:mysql://localhost:3306/gymdata";
+        String user = "root";
+        String password = "";
+        String driver = "com.mysql.cj.jdbc.Driver";
+
+        try {
+            Class.forName(driver);
+            Connection connection = DriverManager.getConnection(url, user, password);
+            System.out.println("good");
+        } catch (ClassNotFoundException e) {
+            System.out.println("JDBC Driver not found!");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("Database connection failed!");
+            e.printStackTrace();
+        }
     }
-    
-    
-}
 }
